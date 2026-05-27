52d13f78a5f0a02d2cb9abd662d67a22c101359f
PrathameshBachhav2005 <professionalworking50@gmail.com>
2026-05-28 00:47:24 +0530

    "simple routes"

--- AuthPortal-Platform/Backend/server.js
+++ AuthPortal-Platform/Backend/server.js
@@ -6,18 +6,20 @@ import mongodb from "./Models/db.js"
 import AuthRouter from "./Routes/AuthRouter.js"
 import ProductRouter from "./Routes/ProductRouter.js"
 
-// mongodb();
-
-const app=express ();
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+// mongodb();
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
 
-app.use("/auth",AuthRouter);
-app.use("/product",ProductRouter);
+app.use("/auth", AuthRouter);
+app.use("/product", ProductRouter);
 
-const Port=process.env.PORT || 8080;
+const Port = process.env.PORT || 8080;
 
-app.listen(Port,()=>{
+app.listen(Port, () => {
     console.log(`Server is running on port ${Port}`);
 })
 
