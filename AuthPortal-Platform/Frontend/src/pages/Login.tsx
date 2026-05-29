6906070dd048652e399d4ea23d4e1661a786f27c
PrathameshBachhav2005 <prathameshbachhav13355@gmail.com>
2026-05-29 14:53:26 +0530

    Update login API URL to production endpoint

--- AuthPortal-Platform/Frontend/src/pages/Login.tsx
+++ AuthPortal-Platform/Frontend/src/pages/Login.tsx
@@ -35,8 +35,8 @@ const Login = () => {
       return toastError("All Fleid are Required!")
     }
     try {
-      const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
-      const url = `${API_URL}/auth/login`;
+      
+      const url = "https://auth-portal-backend.vercel.app//auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
